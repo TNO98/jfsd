1d9a747cff3839075f9cf03d082b1844d48179a2
malay_sarkar <malay.cse@gmail.com>
2022-05-16 01:52:51 +0530

    2nd commit: results are rounded off

--- src/com/iitkanpur/weekend2/Calculator.java
+++ src/com/iitkanpur/weekend2/Calculator.java
@@ -18,21 +18,22 @@ class Calculate{
                 + "5. exit");
         int n = sc.nextInt();
         
+        // used prinf() to roundoff the result using formatting output
         if(n==1) {
             double addition = num1+num2;
-            System.out.println("The addition result is: \n "+num1+"+"+num2+"="+addition);
+            System.out.printf("The addition result is: \n "+num1+"+"+num2+" = "+"%.2f"+"\n",addition);
         }
         else if(n==2) {
             double subtraction = num1-num2;
-            System.out.println("The subtraction result is: \n "+num1+"-"+num2+"="+subtraction);
+            System.out.printf("The subtraction result is: \n "+num1+"-"+num2+" = "+"%.2f"+"\n",subtraction);
         }
         else if(n==3) {
             double multiplication = num1*num2;
-            System.out.println("The multiplication result is: \n "+num1+"x"+num2+"="+multiplication);
+            System.out.printf("The multiplication result is: \n "+num1+"x"+num2+" = "+"%.2f"+"\n",multiplication);
         }
         else if(n==4) {
             double division = num1/num2;
-            System.out.println("The subtraction result is: \n"+num1+"/"+num2+"="+division);
+            System.out.printf("The Division result is: \n"+num1+"/"+num2+" = " +"%.2f"+"\n",division);
         }
         
         else {
